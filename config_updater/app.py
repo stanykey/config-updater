e2c5a72fe3ce888703972d62912671808be672c5
Sergii Lovygin <sergey.lovygin@yahoo.com>
2023-05-06 23:00:30 +0300

    refactor a bit the stub of the main script
    - the changes improve the testability:
         - components can be tested almost independently
         - core functions arguments can be mocked
    - added tests stubs

--- config_updater/app.py
+++ config_updater/app.py
@@ -1,68 +1,114 @@
 """Simple tool to update/override cfg (ini) files from build scripts or CI pipelines via parameters."""
+from argparse import Action
 from argparse import ArgumentParser
 from argparse import Namespace
 from configparser import ConfigParser
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from sys import argv
+from sys import stderr
 from typing import Generator
 from typing import List
-from typing import Tuple
+from typing import Optional
+from typing import Sequence
+from typing import Union
+from typing.io import TextIO
 
 
-def get_arguments() -> Namespace:
+@dataclass
+class Field:
+    name: str
+    value: str = ""
+
+
+class ParseField(Action):
+    def __call__(
+        self,
+        parser: ArgumentParser,
+        namespace: Namespace,
+        values: Union[str, Sequence[str], None],
+        option_string: Optional[str] = None,
+    ) -> None:
+        if values is None:
+            raise ValueError("field value is None")
+
+        fields = []
+        for value in values:
+            key, sep, value = value.partition("=")
+            if not sep:
+                raise ValueError("invalid field value format")
+
+            fields.append(Field(key, value))
+
+        setattr(namespace, self.dest, fields)
+
+
+@dataclass
+class Arguments:
+    file: Path
+    fields: List[Field]
+
+
+def get_arguments(args: Sequence[str]) -> Arguments:
     """Parse command-line arguments and return them."""
     parser = ArgumentParser(prog="config-updater")
     parser.add_argument("file", type=Path, help="Config filename")
     parser.add_argument(
         "params",
-        type=str,
+        action=ParseField,
         metavar="key=value",
         nargs="+",
         help="List of key-value pairs to override config values",
     )
-    return parser.parse_args(argv[1:])
+
+    options = parser.parse_args(args)
+    return Arguments(file=options.file, fields=options.params)
 
 
-def parse_values(raw_values: List[str]) -> List[Tuple[str, str]]:
-    """Parse `raw_values` into list of tuples (key, value)."""
-    return [(key, value) for raw_value in raw_values for key, _, value in [raw_value.partition("=")]]
-
-
-def load_config(file: Path) -> ConfigParser:
+def load_config(file: TextIO) -> ConfigParser:
     """Load config from the `file`."""
     config = ConfigParser()
-    config.read(file, encoding="utf-8")
+    config.read_file(file)
     return config
 
 
-def update_config(config: ConfigParser, values: List[Tuple[str, str]]) -> None:
+def update_config(config: ConfigParser, values: List[Field]) -> None:
     """Update (override) config with **values** from the **list**."""
     # TODO: add main magic here
     pass
 
 
-def save_config(config: ConfigParser, file: Path) -> None:
+def save_config(config: ConfigParser, file: TextIO) -> None:
     """Save `config` to the `file`."""
-    with open(file, "w", encoding="utf-8") as cfg_file:
-        config.write(cfg_file)
+    config.write(file)
 
 
 @contextmanager
 def config_file(file: Path) -> Generator[ConfigParser, None, None]:
-    config = load_config(file)
+    """Simple context manager to load/save config files."""
+    with open(file, encoding="utf-8") as io:
+        config = load_config(io)
+
     try:
         yield config
-    finally:
-        save_config(config, file)
+    except RuntimeError:
+        return
+
+    with open(file, "w", encoding="utf-8") as io:
+        save_config(config, io)
 
 
 def main() -> None:
     """Application entry point."""
-    args = get_arguments()
-    values = parse_values(raw_values=args.params)
+    try:
+        args = get_arguments(argv[1:])
+    except ValueError as error:
+        print(error, file=stderr)
+        exit(1)
+
     with config_file(args.file) as config:
-        update_config(config, values)
+        update_config(config, args.fields)
 
 
 if __name__ == "__main__":
---
+++ tests/config.py
@@ -0,0 +1,17 @@
+from unittest import main
+from unittest import TestCase
+
+
+class ConfigTest(TestCase):
+    def test_basics(self) -> None:
+        self.assertEqual(True, False)
+
+    def test_context_manager(self) -> None:
+        self.assertEqual(True, False)
+
+    def test_update_config(self) -> None:
+        self.assertEqual(True, False)
+
+
+if __name__ == "__main__":
+    main()
